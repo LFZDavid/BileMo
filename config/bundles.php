93616451c0b135cef995120a3947d47a3ee2535b
lfzdavid <lemaagicien@gmail.com>
2021-05-14 16:38:06 +0200

    add Product controller & first test

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['all' => true],
 ];
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -3,15 +3,25 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 
 class ApiTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        // $client = static::createClient();
-        // $crawler = $client->request('GET', '/');
 
-        // $this->assertResponseIsSuccessful();
-        // $this->assertSelectorTextContains('h1', 'Hello World');
+    use FixturesTrait;
+    private $client;
+
+    /**
+     * This method is called before each test.
+     */
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testProductList(): void
+    {
+        $this->client->request('GET','/api/products');
+        $this->assertResponseIsSuccessful();
     }
 }
