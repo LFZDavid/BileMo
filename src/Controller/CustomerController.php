a490a9be182f5b62d29489a9e7862ae4a11e3a12
lfzdavid <lemaagicien@gmail.com>
2021-05-21 14:02:56 +0200

    comment

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -12,22 +12,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CustomerController extends AbstractController
 {
     /**
-     * @Route("/customer", name="customer")
-     */
-    public function index(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CustomerController.php',
-        ]);
-    }
-
-    /**
      * @Route("/api/customers", name="get_customers", methods={"GET"})
      */
     public function customerList(CustomerRepository $customerRepository, SupplierRepository $supplierRepository): Response
     {
+        /** Replace it when auth wil be implemented */
         $supplier = $supplierRepository->findOneBy(["name" => "Orange"]);
+
         return $this->json($customerRepository->findBy(["supplier" => $supplier->getId()]), 200, [], ["groups" => "customer_list"]);
     }
 }
