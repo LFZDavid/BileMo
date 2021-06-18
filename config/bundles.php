277113d89851b57eb639f2cbd6823d0e7a096330
lfzdavid <lemaagicien@gmail.com>
2021-06-18 11:00:56 +0200

    add Nelmio package & annotations for ProductController

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
